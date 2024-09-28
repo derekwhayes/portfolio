d8521c63bfa2936ad088a1ec831504e42b061467
Derek Hayes <derek2099@gmail.com>
2024-09-28 12:05:34 -0500

    Add link to arrow

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-const downArrow = document.querySelector('.down-arrow');
-
-window.addEventListener('load', () => {
-    console.log("EVENT TRIGGER");
-    setTimeout(() => {
-        downArrow.setAttribute('opacity', '1');
-    }, 3000);
-});
